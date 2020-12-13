fb85a6f5447272b5a5867e6d7622225a6b02e65b
KishanSinghChauhan <kishansingh705446@gmail.com>
2020-12-13 13:51:22 +0530

    add index.js with run server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello');
+const express = require('express');
+const socketio = require('socket.io');
+
+const http = require('http');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+const server = http.Server(app);
+const io = socketio(server);
+
+
+server.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`)
+})
+
+
+
+
